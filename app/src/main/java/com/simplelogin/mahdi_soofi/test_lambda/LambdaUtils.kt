e84097ab45e18dfbfe5ea17610be7a2318b69657
mehdiyari <mehdi.email.info@gmail.com>
2018-08-22 17:15:10 +0430

    commit: add extension function for SeekBar  onSeekBarChangeListener with lambda inputs

--- app/src/main/java/com/simplelogin/mahdi_soofi/test_lambda/LambdaUtils.kt
+++ app/src/main/java/com/simplelogin/mahdi_soofi/test_lambda/LambdaUtils.kt
@@ -1,2 +1,23 @@
 package com.simplelogin.mahdi_soofi.test_lambda
 
+import android.widget.SeekBar
+
+fun SeekBar.onSeekBarChangeListener(onProgressChanged: (seekBar: SeekBar?, value: Int, p2: Boolean) -> Unit,
+                                    onStartTrackingTouch:(seekBar: SeekBar?)-> Unit,
+                                    onStopTrackingTouch :(seekBar: SeekBar?) -> Unit)
+{
+    this.setOnSeekBarChangeListener (object: SeekBar.OnSeekBarChangeListener
+    {
+        override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            onProgressChanged(p0, p1, p2)
+        }
+
+        override fun onStartTrackingTouch(p0: SeekBar?) {
+            onStartTrackingTouch(p0)
+        }
+
+        override fun onStopTrackingTouch(p0: SeekBar?) {
+            onStopTrackingTouch(p0)
+        }
+    })
+}
